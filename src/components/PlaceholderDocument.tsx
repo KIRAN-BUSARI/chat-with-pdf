671a1c58f25087aa61f315937bef13a93abe4740
KIRAN-BUSARI <kiranbusari2208@gmail.com>
2024-07-24 12:41:27 +0530

    Add layout and header

---
+++ src/components/PlaceholderDocument.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { PlusCircleIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+function PlaceholderDocument() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // Check if user is FREE tier or PRO tier
+    // If FREE tier, redirect to pricing page
+    // If PRO tier, redirect to upload page
+    router.push("/dashboard/upload");
+  };
+  return (
+    <Button
+      onClick={handleClick}
+      className="flex flex-col items-center w-64 h-80 rounded-xl bg-gray-200 drop-shadow-md text-gray-400"
+    >
+      <PlusCircleIcon className="h-16 w-16" />
+      <p>Add a document</p>
+    </Button>
+  );
+}
+
+export default PlaceholderDocument;
